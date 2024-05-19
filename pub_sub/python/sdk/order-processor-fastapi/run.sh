ab5c77051bab4307768e5915632f86c9778a73e4
Simon Stipcich <simon.stipcich@derivco.co.za>
2024-05-19 10:22:19 +0200

    fix:dapr run all script.

--- pub_sub/python/sdk/order-processor-fastapi/run.sh
+++ pub_sub/python/sdk/order-processor-fastapi/run.sh
@@ -4,4 +4,7 @@
 
 # dapr run --app-id order-processor --resources-path ../../../components/ --app-port 6002 -- python3 app.py
 
-dapr run --app-id order-processor --resources-path ../../../components/ --app-port 6002 -- python3 -m uvicorn app:app --host 0.0.0.0 --port 6002
+dapr run --app-id order-processor \
+    --resources-path ../../../components/ \
+    --app-port 6002 \
+    -- python3 -m uvicorn app:app --host 0.0.0.0 --port 6002
